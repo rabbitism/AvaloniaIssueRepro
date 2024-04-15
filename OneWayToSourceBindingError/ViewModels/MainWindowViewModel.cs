3ccf48f3427648639630c3d3c950f85fcceeb1e7
rabbitism <dongbin@irihi.tech>
2024-04-15 16:02:02 +0800

    feat: add repro.

---
+++ OneWayToSourceBindingError/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace OneWayToSourceBindingError.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    private decimal _decimalValue;
+
+    public decimal DecimalValue
+    {
+        get => _decimalValue;
+        set => SetProperty(ref _decimalValue, value);
+    }
+}
